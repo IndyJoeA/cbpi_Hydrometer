6ffe63037475dd41c057dab345464e7d12ef6370
HamOP <stefan@stefanspretina7.speedport.ip>
2018-10-05 23:27:50 +0200

    Added alcohol content
    Added calculation of alcohol content. Original wort is configured in sensor settings.

--- __init__.py
+++ __init__.py
@@ -8,12 +8,14 @@ import json
 from flask import Blueprint, render_template, jsonify, request
 from modules.core.props import Property
 
-blueprint = Blueprint('ispindel', __name__)
+blueprint = Blueprint('ispindel_alccalc', __name__)
 cache = {}
 
 def calcGravity(polynom, tilt, unitsGravity):
 	if unitsGravity == "SG":
 		rounddec = 3
+	elif unitsGravity =="ALC":
+		rounddec = 10
 	else:
 		rounddec = 2
 
@@ -22,13 +24,20 @@ def calcGravity(polynom, tilt, unitsGravity):
 	result = eval(polynom)
 	result = round(float(result),rounddec)
 	return result
+	
+def calcAlcohol(gravity, origWort):
+	realGravity = 0.1808*origWort + 0.8192*gravity
+	result = (origWort-realGravity)/(2.0665-0.010665*origWort)*1.267
+	result = round(float(result), 2)
+	return result
 
 @cbpi.sensor
-class iSpindel(SensorActive):
+class iSpindel_AlcCalc(SensorActive):
 	key = Property.Text(label="iSpindel Name", configurable=True, description="Enter the name of your iSpindel")
-	sensorType = Property.Select("Data Type", options=["Temperature", "Gravity", "Battery"], description="Select which type of data to register for this sensor")
+	sensorType = Property.Select("Data Type", options=["Temperature", "Gravity", "Battery", "Alcohol Content"], description="Select which type of data to register for this sensor")
 	tuningPolynom = Property.Text(label="Tuning Polynomial", configurable=True, default_value="tilt", description="Enter your iSpindel polynomial. Use the variable tilt for the angle reading from iSpindel. Does not support ^ character.")
 	unitsGravity = Property.Select("Gravity Units", options=["SG", "Brix", "°P"], description="Displays gravity reading with this unit if the Data Type is set to Gravity. Does not convert between units, to do that modify your polynomial.")
+	originalWort = Property.Text(label="Original Wort (°P)", configurable = True, description="Enter original wort in °P for alcohol percentage calculation")
 
 	def get_unit(self):
 		if self.sensorType == "Temperature":
@@ -37,6 +46,8 @@ class iSpindel(SensorActive):
 			return self.unitsGravity
 		elif self.sensorType == "Battery":
 			return "V"
+		elif self.sensorType == "Alcohol Content":
+			return "Vol-%"
 		else:
 			return " "
 
@@ -50,6 +61,9 @@ class iSpindel(SensorActive):
 				if cache[self.key] is not None:
 					if self.sensorType == "Gravity":
 						reading = calcGravity(self.tuningPolynom, cache[self.key]['Angle'], self.unitsGravity)
+					elif self.sensorType == "Alcohol Content":
+					    reading = calcGravity(self.tuningPolynom, cache[self.key]['Angle'], "ALC")
+					    reading = calcAlcohol(reading, float(self.originalWort))
 					else:
 						reading = cache[self.key][self.sensorType]
 					self.data_received(reading)
@@ -73,5 +87,5 @@ def set_temp():
 
 @cbpi.initalizer()
 def init(cbpi):
-	print "INITIALIZE ISPINDEL MODULE"
+	print "INITIALIZE ISPINDEL ALCCALC MODULE"
 	cbpi.app.register_blueprint(blueprint)
